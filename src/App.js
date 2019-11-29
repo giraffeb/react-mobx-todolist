25c1e942769d3ed1d8b0e3c6aae7fb394998fdda
giraffeb <rwpakr07@gmail.com>
2019-11-29 16:55:31 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DevTools from 'mobx-react-devtools';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <DevTools />
     </div>
   );
 }
